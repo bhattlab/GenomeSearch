acc3c66bddc2832360848cbdbb29ee36d89da4ba
durrantmm <mdurrant@Matts-MacBook-Pro-3.local>
2020-07-31 10:15:37 -0600

    updated version

--- genomesearch/__init__.py
+++ genomesearch/__init__.py
@@ -1,6 +1,6 @@
 from os.path import join, dirname, isfile
 
-__version__ = '0.0.4_dev'
+__version__ = '0.0.5_dev'
 
 REFBANK_SQLDB_PATH = join(dirname(__file__), 'data/refbank_genomesearch.db')
 UHGG_SQLDB_PATH = join(dirname(__file__), 'data/uhgg_genomesearch.db')
